c6c1d52d3b32ce5998a958c105886121071c0b34
Ermal Vrapi <ermalvrapi@hp-elitebook-ermal-vrapi.marostica.crispy.intra>
2019-11-22 12:51:01 +0100

    added remove func

--- src/inline-block/block.js
+++ src/inline-block/block.js
@@ -1,20 +1,20 @@
-import './editor.scss';
-import './style.scss';
-import { __ } from '@wordpress/i18n';
-import { Fragment } from '@wordpress/element';
-import { registerBlockType } from '@wordpress/blocks';
+import "./editor.scss";
+import "./style.scss";
+import { __ } from "@wordpress/i18n";
+import { Fragment } from "@wordpress/element";
+import { registerBlockType } from "@wordpress/blocks";
 import {
 	RichText,
 	MediaUpload,
 	InspectorControls,
-	ColorPalette,
-} from '@wordpress/block-editor';
+	ColorPalette
+} from "@wordpress/block-editor";
 import {
 	Button,
 	PanelBody,
 	RangeControl,
-	ToggleControl,
-} from '@wordpress/components';
+	ToggleControl
+} from "@wordpress/components";
 
 /**
  *
@@ -24,50 +24,50 @@ import {
  * @return {?WPBlock}          The block, if it has been successfully
  *                             registered; otherwise `undefined`.
  */
-registerBlockType( 'guten-block/inline', {
-	title: __( 'Inline Block' ),
-	icon: 'menu',
-	category: 'guten-blocks',
+registerBlockType("guten-block/inline", {
+	title: __("Inline Block"),
+	icon: "editor-insertmore",
+	category: "guten-blocks",
 	attributes: {
 		title: {
-			type: 'string',
-			source: 'html',
-			selector: 'h3',
+			type: "string",
+			source: "html",
+			selector: "h3"
 		},
 		titleColor: {
-			type: 'sring',
-			default: 'black',
+			type: "sring",
+			default: "black"
 		},
 		contentColor: {
-			type: 'sring',
-			default: 'black',
+			type: "sring",
+			default: "black"
 		},
 		borderColor: {
-			type: 'sring',
-			default: 1,
+			type: "sring",
+			default: 1
 		},
 		borderWidth: {
-			type: 'sring',
-			default: 0,
+			type: "sring",
+			default: 0
 		},
 		mediaID: {
-			type: 'number',
+			type: "number"
 		},
 		mediaURL: {
-			type: 'string',
-			source: 'attribute',
-			selector: 'img',
-			attribute: 'src',
+			type: "string",
+			source: "attribute",
+			selector: "img",
+			attribute: "src"
 		},
 		content: {
-			type: 'string',
-			source: 'html',
-			selector: 'p',
+			type: "string",
+			source: "html",
+			selector: "p"
 		},
 		isDisabled: {
-			type: 'boolean',
-			source: false,
-		},
+			type: "boolean",
+			value: false
+		}
 	},
 
 	/**
@@ -88,123 +88,121 @@ registerBlockType( 'guten-block/inline', {
 				contentColor,
 				borderColor,
 				borderWidth,
-				isDisabled,
+				isDisabled
 			},
-			setAttributes,
+			setAttributes
 		} = props;
 
 		const onChangeTitle = value => {
-			setAttributes( { title: value } );
+			setAttributes({ title: value });
 		};
 
 		const onSelectImage = media => {
-			setAttributes( {
+			setAttributes({
 				mediaURL: media.url,
-				mediaID: media.id,
-			} );
+				mediaID: media.id
+			});
 		};
 		const onChangeContent = value => {
-			setAttributes( { content: value } );
+			setAttributes({ content: value });
 		};
 
 		const onChangeTitleColor = value => {
-			setAttributes( { titleColor: value } );
+			setAttributes({ titleColor: value });
 		};
 
 		const onChangeContentColor = value => {
-			setAttributes( { contentColor: value } );
+			setAttributes({ contentColor: value });
 		};
 
 		const onChangeBorderColor = value => {
-			setAttributes( { borderColor: value } );
+			setAttributes({ borderColor: value });
 		};
 		const onChangeBorderWidth = value => {
-			setAttributes( { borderWidth: value } );
+			setAttributes({ borderWidth: value });
 		};
 
 		const onHideBlock = () => {
-			setAttributes( { isDisabled: ! isDisabled } );
-			console.log( attributes.isDisabled );
+			setAttributes({ isDisabled: !isDisabled });
+			// console.log( attributes.isDisabled );
 		};
 
 		return (
 			<Fragment>
 				<InspectorControls>
-					<PanelBody initialOpen={ false } title={ 'Font Color Settings' }>
+					<PanelBody initialOpen={false} title={"Font Color Settings"}>
 						<p>Select Title Color</p>
-						<ColorPalette value={ titleColor } onChange={ onChangeTitleColor } />
+						<ColorPalette value={titleColor} onChange={onChangeTitleColor} />
 
 						<p>Select Content Color</p>
 						<ColorPalette
-							value={ contentColor }
-							onChange={ onChangeContentColor }
+							value={contentColor}
+							onChange={onChangeContentColor}
 						/>
 					</PanelBody>
 
-					<PanelBody initialOpen={ false } title={ 'Box Border ' }>
+					<PanelBody initialOpen={false} title={"Box Border "}>
 						<p>Select Border Width</p>
 						<RangeControl
 							label="Border Width"
-							value={ borderWidth }
-							onChange={ onChangeBorderWidth }
+							value={borderWidth}
+							onChange={onChangeBorderWidth}
 						/>
 						<p>Select Border Color</p>
-						<ColorPalette value={ borderColor } onChange={ onChangeBorderColor } />
+						<ColorPalette value={borderColor} onChange={onChangeBorderColor} />
 					</PanelBody>
-					<PanelBody initialOpen={ false } title={ 'Hide Block ' }>
+					<PanelBody initialOpen={false} title={"Hide Block "}>
 						<ToggleControl
 							label="Hide Block"
-							checked={ isDisabled }
-							onChange={ onHideBlock }
+							onChange={onHideBlock}
+							checked={isDisabled}
 						/>
 					</PanelBody>
 				</InspectorControls>
 
-				<div className={ className }>
-					<div
-						className="inline__block_container"
-						onChange={ ( onChangeBorderColor, onChangeBorderWidth ) }
-						style={ { border: borderWidth + 'px ' + 'solid ' + borderColor } }
-					>
-						<div className="inline__block-content">
-							<RichText
-								tagName="h3"
-								placeholder={ __( 'Write Title Here', 'guten-blocks' ) }
-								value={ title }
-								onChange={ onChangeTitle }
-								className="inline__block-title"
-								style={ { color: titleColor } }
-							/>
-							<RichText
-								tagName="p"
-								placeholder={ __( 'Write Content Here', 'guten-blocks' ) }
-								value={ content }
-								onChange={ onChangeContent }
-								style={ { color: contentColor } }
-							/>
-						</div>
-						<div className="inline__block-image">
-							<MediaUpload
-								onSelect={ onSelectImage }
-								allowedTypes="image"
-								value={ mediaID }
-								render={ ( { open } ) => (
-									<Button
-										className={ mediaID ? 'image-button' : 'button button-large' }
-										onClick={ open }
-									>
-										{ ! mediaID ? (
-											__( 'Upload Image', 'guten-blocks' )
-										) : (
-											<img
-												src={ mediaURL }
-												alt={ __( 'Upload Block Image', 'guten-blocks' ) }
-											/>
-										) }
-									</Button>
-								) }
-							/>
-						</div>
+				<div
+					className={className}
+					onChange={(onChangeBorderColor, onChangeBorderWidth)}
+					style={{ border: borderWidth + "px " + "solid " + borderColor }}
+				>
+					<div>
+						<RichText
+							tagName="h3"
+							placeholder={__("Write Title Here", "guten-blocks")}
+							value={title}
+							onChange={onChangeTitle}
+							className="inline__block-title"
+							style={{ color: titleColor }}
+						/>
+						<RichText
+							tagName="p"
+							placeholder={__("Write Content Here", "guten-blocks")}
+							value={content}
+							onChange={onChangeContent}
+							style={{ color: contentColor }}
+						/>
+					</div>
+					<div>
+						<MediaUpload
+							onSelect={onSelectImage}
+							allowedTypes="image"
+							value={mediaID}
+							render={({ open }) => (
+								<Button
+									className={mediaID ? "image-button" : "button button-large"}
+									onClick={open}
+								>
+									{!mediaID ? (
+										__("Upload Image", "guten-blocks")
+									) : (
+										<img
+											src={mediaURL}
+											alt={__("Upload Block Image", "guten-blocks")}
+										/>
+									)}
+								</Button>
+							)}
+						/>
 					</div>
 				</div>
 			</Fragment>
@@ -228,34 +226,35 @@ registerBlockType( 'guten-block/inline', {
 				contentColor,
 				borderColor,
 				borderWidth,
-			},
+				isDisabled
+			}
 		} = props;
 		return (
-			<div className={ className }>
-				<div
-					className="inline__block_container"
-					style={ { border: borderWidth + 'px ' + 'solid ' + borderColor } }
-				>
-					<div className="inline__block-content">
-						<RichText.Content
-							tagName="h3"
-							value={ title }
-							className="inline__block-title"
-							style={ { color: titleColor } }
-						/>
-						<RichText.Content
-							tagName="p"
-							value={ content }
-							style={ { color: contentColor } }
-						/>
-					</div>
-					<div className="inline__block-image">
-						{ mediaURL && (
-							<img src={ mediaURL } alt={ __( 'Block Image', 'guten-blocks' ) } />
-						) }
-					</div>
+			<div
+				className={`inline__block_container ${className} ${
+					isDisabled ? "hidden" : "visible"
+				}`}
+				style={{ border: borderWidth + "px " + "solid " + borderColor }}
+			>
+				<div className="inline__block-content">
+					<RichText.Content
+						tagName="h3"
+						value={title}
+						className="inline__block-title"
+						style={{ color: titleColor }}
+					/>
+					<RichText.Content
+						tagName="p"
+						value={content}
+						style={{ color: contentColor }}
+					/>
+				</div>
+				<div className="inline__block-image">
+					{mediaURL && (
+						<img src={mediaURL} alt={__("Block Image", "guten-blocks")} />
+					)}
 				</div>
 			</div>
 		);
-	},
-} );
+	}
+});
